baa82bae6db5ab10e112cc892014e596811ae257
amogusussy <83502633+amogusussy@users.noreply.github.com>
2022-08-13 21:00:03 +0100

    Add files via upload
    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function startTime() {
+    const date = new Date();
+    var h = date.getHours();
+    var m = date.getMinutes();
+    var s = date.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+} function checkTime(i) {if (i < 10) {i = "0" + i}; return i;}
