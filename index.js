013d173eea395ae97b9bbd5e85188d82d955ef30
amogusussy <83502633+amogusussy@users.noreply.github.com>
2022-08-14 02:36:41 +0100

    Fix hour < 10

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ function startTime() {
     var h = date.getHours();
     var m = date.getMinutes();
     var s = date.getSeconds();
+    if (h == 0){h=12;} 
+    else if (h<10){h = "0" + String(h);}
     m = checkTime(m);
     s = checkTime(s);
     document.getElementById('time').innerHTML = h + ":" + m + ":" + s;
